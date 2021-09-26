85c7f3d5f362c6c952878fccd561f31af27644d1
MrTux31 <89023193+MrTux31@users.noreply.github.com>
2021-09-26 15:53:54 +0200

    adding music

--- main.py
+++ main.py
@@ -2,8 +2,12 @@
 # coding:utf-8
 import pygame
 from game import Game
+from pygame import mixer
 
 if __name__ == '__main__':
     pygame.init()
+    # jouer la musique en boucle
+    mixer.music.load('Songs/The Escape Official Theme Song.ogg ')
+    mixer.music.play(-1)
     game = Game()
     game.run()
